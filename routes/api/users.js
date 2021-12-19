d766d51aa2497b784ead54462c1783f266f4b7b0
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2021-12-19 20:54:56 +0530

    user authentication & login routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -61,8 +63,21 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      response.send("User Registerd");
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (error, token) => {
+          if (error) throw error;
+          response.json({ token });
+        }
+      );
     } catch (error) {
       console.log(error.message);
       response.status(500).send("Server Error");
